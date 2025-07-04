64f34265e8cfdfbd22cff1a82381161220c1ac85
lukavukadin <luka.vukadin7@gmail.com>
2025-07-04 21:30:25 +0200

    UPDATE320940923

--- kanban-board/backend/routes/tasks.js
+++ kanban-board/backend/routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Task = require('../models/Task');
+const Task = require('../models/task');
 
 // GET: Alle Tasks abrufen
 router.get('/', async (req, res) => {
